be26af65f9f94f01e145a324ab3dc62bc911f687
Ytalo Lopes <suporte@ytalo.dev>
2023-09-23 17:56:52 -0300

    Adicionando botao contato

--- src/app/components/Jobs.tsx
+++ src/app/components/Jobs.tsx
@@ -81,12 +81,11 @@ export default function Jobs() {
                 </div>
               </div>
 
-              {/* <a href={`https://wa.me/55${post.author.whatsapp}`}
+              <a href={`https://wa.me/55${post.author.whatsapp}`}
                 className="mt-10 block w-full rounded-md bg-green-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
               >
-                <span className="absolute inset-0" />
                 Contato
-              </a> */}
+              </a>
             </article>
           ))}
         </div>
