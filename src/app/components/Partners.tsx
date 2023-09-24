487634cba5dc7545b5e0c4e836ee75490d9856e3
ytalotech <lopessoftec@gmail.com>
2023-09-23 21:54:00 -0300

    Alteracao na importacao imagem

--- src/app/components/Partners.tsx
+++ src/app/components/Partners.tsx
@@ -41,7 +41,7 @@ export default function Partners() {
         </div>
 
         {informationsPartners.map((information) => (
-          <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+          <div key={information.id} className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
             <div className="p-8 sm:p-10 lg:flex-auto">
               <h3 className="text-2xl font-bold tracking-tight text-gray-900">{information.name}</h3>
               <p className="mt-6 text-base leading-7 text-gray-600">
